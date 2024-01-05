ab751808435c6ba7a012eceb73cedde19ff055d9
ttesche <tiago.tesche@gmail.com>
2024-01-05 12:46:46 -0300

    -VALIDATION OF INPUTS IN STRESS CLASS. ttesche

--- Stress.cs
+++ Stress.cs
@@ -10,72 +10,91 @@ namespace MechanicsMaterials
     /* REFERENCE: https://mechcollege.com/stress-types/ */
     /* STRESSES CATHEGORIZED - >  ALTHOUGH IT'S THE SAME FORMULAS, I HAVE SEPARATE IN DIFFERENTES METHODS.*/
     {
-        public double Normal(double area_, double force_)
-        {            
+        public double Normal(double area, double force)
+        {
             /*When a force acts perpendicular (or "normal") to the surface of an object, it exerts a normal stress */
-            /* area_ -> crossectinal area
+            /* area_ -> crossectional area
              * force_ -> normal force being applied */
-            return force_ / area_;
+            ValidateInputs(area, force);
+
+            return force / area;
 
         }
 
-        public double Shear(double area_, double force_)
+        public double Shear(double area, double force)
         {
             /* Shear stress is the resistance to deform due to perpendicular forces operating along with the surface */
             /* area_ -> crossectinal area
              * force_ -> perpendicular force being applied */
-            return force_ / area_;
+            ValidateInputs(area, force);
+            return force / area;
 
         }
 
-        public double Bending(double moment_, double y_, double i_)
+        public double Bending(double moment, double y, double inertia)
         {
 
             /* Bending stress is the resistance of an object to bend.In bending, the force is a normal load and applied at a specific spot of the object */
             /* moment_ -> bending moment being applied
              * y_ -> vertical distance from neutral axis
              * i_-> moment of inertia */
-            double equation = (moment_ * y_) / i_;
+            ValidateInputs(moment, y, inertia);
+            double equation = (moment * y) / inertia;
             return equation;
 
         }
-        public double Bending(double f_, double x_, double y_, double I_) 
+        public double Bending(double force, double distance, double y, double inertia) 
         {
             /* ANOTHER CALL FOR THE BENDING STRESS METHOD UTILIZING f_ AND x_ TO CALCULATE BENDING MOMENT */
             /* f_ -> force that composes bending moment being applied
              * x_ -> distance that composes bending moment being applied
              * y_ -> vertical distance from neutral axis
              * i_-> moment of inertia */
-            double moment = f_ * x_;
-            double equation = (moment * y_) / I_;
+            ValidateInputs(force, distance, y, inertia);
+            double moment = force * distance;
+            double equation = (moment * y_) / inertia;
             return equation;
 
         }
 
-        public  double Torsional(double t_, double r_, double j_)
+        public  double Torsional(double torque, double radius, double polarMoment)
 
         {
             /*Torsional stress is shear stress caused due to twisting. In other words, it may be described as the angular deformation of a body. 
             * t_ -> torque apllied to the section
             * r_ -> radius of section
             * j_ -> polar moment of inertia */
+            ValidateInputs(torque, radius, polarMoment);
+            if (polarMoment == 0)
+                throw new ArgumentException("Polar moment of inertia must be non-zero.");
 
-            double equation = (t_ * r_) / j_;   
+            double equation = (torque * radius) / polarMoment;
+           
             return equation;   
 
         }
-        public  double Torsional_2(double g_, double theta_, double i_)
+        public  double Torsional_2(double modulusOfRigidity, double angleOfTwist, double inertia)
         {
 
             /*Torsional stress is shear stress caused due to twisting. In other words, it may be described as the angular deformation of a body. 
             * g_ -> Modulus of Rigidity
             * theta_ -> Angle of Twist
             * i_ -> moment of inertia */
-
-            double theta = (Math.PI/180) *theta_;
-            double equation = (g_*theta)/i_;
+            ValidateInputs(modulusOfRigidity, angleOfTwist, inertia);
+            double theta = (Math.PI/180) *angleOfTwist;
+            double equation = (modulusOfRigidity*theta)/inertia;
             
             return equation;
         }
+
+        private void ValidateInputs(params double[] values)
+        {
+            foreach (double value in values)
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Input values must be greater than zero.");
+            }
+        }
+
     }
 }
